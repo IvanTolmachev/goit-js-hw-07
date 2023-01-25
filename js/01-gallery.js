95a4f57ca203d6351c3b340e1a42c85ca8514981
IvanTolmachev <39563900+IvanTolmachev@users.noreply.github.com>
2023-01-25 19:03:20 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const galleryNew = document.querySelector(".basicLightbox");
+
+/* создание елемента галереи*/
+
+const renderListGallery = (makeImageItems) =>
+  makeImageItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<div class="gallery__item"><a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+  </a>
+</div>`,
+    ""
+  );
+
+galleryEl.insertAdjacentHTML("afterbegin", renderListGallery(galleryItems));
+
+/*событие*/
+
+galleryEl.addEventListener("click", onAllGalleryClick);
+
+function onAllGalleryClick(e) {
+  const imageLink = e.target.dataset.source;
+
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const makeImage = `<img
+      src="${imageLink}"
+      width="800"
+      height="600"
+      >`;
+
+  const instance = basicLightbox.create(makeImage, {
+    onShow: () => {
+      window.addEventListener("keydown", onEscKeyClick);
+    },
+    onClose: () => {
+      window.removeEventListener("keydown", onEscKeyClick);
+    },
+  });
+
+  instance.show();
+  function onEscKeyClick(e) {
+    const ESC_KEY_CODE = "Escape";
+    const isEscKey = e.code === ESC_KEY_CODE;
+    if (isEscKey) {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const renderListGallery = (makeImageItems) =>
+  makeImageItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`,
+    ""
+  );
+
+galleryEl.insertAdjacentHTML("afterbegin", renderListGallery(galleryItems));
+galleryEl.addEventListener("click", onAllGalleryClick);
+
+function onAllGalleryClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  new SimpleLightbox(".gallery a", {
+    captionSelector: "img",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
